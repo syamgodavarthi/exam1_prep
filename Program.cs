2e7342615149863a75eb86bbd32f5345110e6e7c
Syam Godavarthi <syamg@usf.edu>
2019-10-06 13:50:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exam1_prep
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = 281;
+            Console.WriteLine(GetMean(n));
+        }
+
+        public static int GetMean(int n)
+        {
+            int sum = 0;
+            string n1 = n.ToString();
+            int count = 0;
+            int m = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                count += 1;
+
+                n = n / 10;
+            }
+            Console.WriteLine(m);
+            m = sum / count;
+            return m;
+        }
+    }
+}
